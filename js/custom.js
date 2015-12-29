de5366dbfa16ae10da1cfeeec142084a831b3bbb
entouraj <metsalldaway1112@gmail.com>
2015-12-29 17:39:48 -0500

    Online Resume
    Online Resume

---
+++ js/custom.js
@@ -0,0 +1,97 @@
+/*$(document).ready(function(){
+	
+$('a[href^="#"]').on('click', function (e) {
+
+        e.preventDefault();
+        $(document).off("scroll");
+         
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+         var target = this.hash,
+         menu = target;
+        $target = $(target);
+               
+       $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 200, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+$(window).scroll(function(){
+        // the "12" should equal the margin-top value for nav.stickydiv
+        var window_top = $(window).scrollTop() + 12; 
+        var div_top = $('.sidebar').offset().top;
+            if (window_top >= div_top) {
+                $('.sidebar').addClass('fixed');
+            } else {
+                $('.sidebar').removeClass('fixed');
+            }
+ });
+
+});*/
+
+
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 100, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+   
+});
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+
